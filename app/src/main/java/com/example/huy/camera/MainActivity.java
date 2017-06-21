8dccbe52ef420b8eb8766ac87343331540914702
Huy Trinh <huy.trinh@neighbourfood.org>
2017-06-21 09:11:38 +0300

    Set up image picker function

--- app/src/main/java/com/example/huy/camera/MainActivity.java
+++ app/src/main/java/com/example/huy/camera/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.huy.camera;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +10,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.mvc.imagepicker.ImagePicker;
+
 public class MainActivity extends AppCompatActivity {
+    private ImageButton imangeButton;
+    private ImageView picture;
+    private Bitmap currentPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +29,36 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        picture = (ImageView) findViewById(R.id.picture);
+        imangeButton = (ImageButton) findViewById(R.id.add_picture);
+        imangeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                onPickImage(v);
             }
         });
+
+    }
+
+    public void onPickImage(View view) {
+        // Click on image button
+        ImagePicker.pickImage(this, "Select your image:");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap bitmap = ImagePicker.getImageFromResult(this, requestCode, resultCode, data);
+        if (bitmap != null){
+            currentPicture = bitmap;
+            picture.setImageBitmap(bitmap);
+        }else{
+            if(currentPicture != null){
+                picture.setImageBitmap(currentPicture);
+            }else{
+                Glide.with(this).load(R.drawable.profile).into(picture);
+            }
+        }
+
     }
 
     @Override
